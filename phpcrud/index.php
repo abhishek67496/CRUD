f9957ec37fe606fd1de61cf9319440809ba32f6f
abhishek67496 <157359421+abhishek67496@users.noreply.github.com>
2024-03-15 13:21:05 +0530

    File Uploaded

---
+++ phpcrud/index.php
@@ -0,0 +1,16 @@
+<!-- Home page for our CRUD app. -->
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+</div>
+
+<?=template_footer()?>
